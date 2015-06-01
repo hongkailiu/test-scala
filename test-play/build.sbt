fd476c336956baf8ea20defdd2beab8ad1773ea7
Hongkai Liu <hongkailiu@users.noreply.github.com>
2015-06-01 17:12:31 -0400

    scalamock in action

--- test-play/build.sbt
+++ test-play/build.sbt
@@ -12,11 +12,12 @@ libraryDependencies ++= Seq(
   "org.webjars" % "bootstrap" % "3.3.4" exclude("org.webjars", "jquery"),
   "org.webjars" % "requirejs" % "2.1.11-1",
   "org.twitter4j" % "twitter4j-core" % "4.0.3",
-  "com.typesafe.akka" %% "akka-testkit" % "2.3.4" % "test"
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.4" % "test",
   //"org.scalatest" %% "scalatest" % "2.2.4" % Test,
   // not working with the following
   //  "org.scalatestplus" %% "play" % "1.1.1" % "test",
   //"org.scalamock" %% "scalamock-scalatest-support" % "3.2" % "test"
+  "org.scalamock" %% "scalamock-specs2-support" % "3.2" % "test"
 )
 
 lazy val root = (project in file(".")).addPlugins(PlayScala)
--- test-play/test/helper/MyActorTest.scala
+++ test-play/test/helper/MyActorTest.scala
@@ -1,36 +1,41 @@
 package helper
 
-import Messages.{MyMessageRequestTwitt, MyMessage, MyMessageResponse}
+import Messages.{MyMessage, MyMessageRequestTwitt, MyMessageResponse}
 import actor.AkkaTestkitSpecs2Support
 import akka.actor.{ActorRef, ActorSystem, Props}
-import org.mockito.Matchers
+import org.scalamock.specs2.IsolatedMockFactory
 import org.specs2.mutable._
 import org.specs2.time.NoTimeConversions
 
 import scala.concurrent.duration._
+
 /**
  * Created by hongkailiu on 2015-05-30.
  */
-class MyActorTest extends Specification with NoTimeConversions with org.specs2.mock.Mockito {
-  sequential // forces all tests to be run sequentially
+class MyActorTest extends Specification with NoTimeConversions with IsolatedMockFactory {
+  sequential
+  // forces all tests to be run sequentially
   val system = ActorSystem("HelloAkkaSpec")
-  val mockTwitterHelper:TwitterHelper = mock[TwitterHelperImpl]
+  val mockTwitterHelper: TwitterHelper = mock[TwitterHelperImpl]
   //does not work yet with spec2 version
   //mockTwitterHelper.request(Matchers.any[ActorRef],Matchers.anyString(),Matchers.anyInt(),Matchers.any[ResultHandler]) returns Unit
   //mockTwitterHelper.request(Matchers.any[ActorRef],Matchers.anyString(),Matchers.anyInt(),Matchers.any[ResultHandler]) returns(())
-  org.mockito.Mockito.doNothing().when(mockTwitterHelper).request(Matchers.any[ActorRef],Matchers.anyString(),Matchers.anyInt(),Matchers.any[ResultHandler])
-
+  //the following one is working, but less cool. NOT SCALA-LIKE and still in java
+  //org.mockito.Mockito.doNothing().when(mockTwitterHelper).request(Matchers.any[ActorRef],Matchers.anyString(),Matchers.anyInt(),Matchers.any[ResultHandler])
+  //so we use scalamock instead
 
   val unitUnderTest = system.actorOf(Props(new MyActor(mockTwitterHelper)), "myActor")
 
   "MyActor" should {
 
     "respond with twitt-request messages" in new AkkaTestkitSpecs2Support(system) {
+      (mockTwitterHelper.request _).expects(*, *, *, *).returning(Unit).once
       within(1 second) {
         val msg = MyMessageRequestTwitt(null.asInstanceOf[ActorRef], null.asInstanceOf[String], 0, null.asInstanceOf[ResultHandler])
         unitUnderTest ! msg
         expectMsgType[MyMessageResponse].message must be equalTo MyMessage.TWITT_DONE_MSG
-        there was one(mockTwitterHelper).request(Matchers.any[ActorRef],Matchers.anyString(),Matchers.anyInt(),Matchers.any[ResultHandler])
+        //there was one(mockTwitterHelper).request(Matchers.any[ActorRef],Matchers.anyString(),Matchers.anyInt(),Matchers.any[ResultHandler])
+
       }
     }
 
@@ -49,6 +54,7 @@ class MyActorTest extends Specification with NoTimeConversions with org.specs2.m
         expectNoMsg()
       }
     }
+
   }
 
 
