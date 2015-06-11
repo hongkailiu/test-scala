2d8e4772899deb344ee25573b81e72e537e8b132
Hongkai Liu <hongkailiu@users.noreply.github.com>
2015-06-11 09:37:22 -0400

    one more test starts

---
+++ test-play/test/helpers/TwitterServiceIT.scala
@@ -0,0 +1,14 @@
+package helpers
+
+import org.specs2.mutable.Specification
+
+/**
+ * Created by ehongka on 6/11/15.
+ */
+class TwitterServiceIT extends Specification {
+  "TwitterService" should {
+    "provide twitts" in {
+      true should beTrue
+    }
+  }
+}
